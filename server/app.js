b649d4f3d8285da6cac3ead787e72b08b1675689
Adam Petroff <adam.petroff@thefuntasty.com>
2018-04-12 13:16:22 +0200

    add sockets

--- server/app.js
+++ server/app.js
@@ -1,11 +1,24 @@
 const path = require('path');
 const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
 const publicPath = path.join(__dirname, '../public');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log('new user');
+
+    socket.on('disconnect', () => {
+        console.log('disconnected ');
+    })
+});
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-    console.log('running on port 3000');
+server.listen(3001, () => {
+    console.log('server is running on port 3001');
 });
